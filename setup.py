1c2b9093f43fecf79f3400e391f77bea983f91e0
NotTheEvilOne <git-NTEO@vplace.de>
2025-05-04 11:32:53 +0200

    Replace "dpt_json" package name with "ppt_json"

--- setup.py
+++ setup.py
@@ -1,79 +1,43 @@
 # -*- coding: utf-8 -*-
 
 """
-direct Python Toolbox
-All-in-one toolbox to encapsulate Python runtime variants
+Personal Python Toolkit
+Modularized all-in-one toolkit for Python
 ----------------------------------------------------------------------------
-(C) direct Netware Group - All rights reserved
-https://www.direct-netware.de/redirect?dpt;json
+(C) Tobias "NotTheEvilOne" Wolf - All rights reserved
+https://github.com/NotTheEvilOne/ppt_json
 
 This Source Code Form is subject to the terms of the Mozilla Public License,
 v. 2.0. If a copy of the MPL was not distributed with this file, You can
 obtain one at http://mozilla.org/MPL/2.0/.
-----------------------------------------------------------------------------
-https://www.direct-netware.de/redirect?licenses;mpl2
-----------------------------------------------------------------------------
-setup.py
 """
 
-from os import makedirs, path
+import os
+from typing import Dict, Any
 
 try:
     from setuptools import find_packages, setup
 except ImportError:
-    from distutils import find_packages, setup
-#
+    from distutils import find_packages, setup  # type: ignore[attr-defined, unused-ignore]
 
-_use_dist_mode = False
 
-try:
-    from dpt_builder_suite.distutils.build_py import BuildPy
-    from dpt_builder_suite.distutils.sdist import Sdist
-    from dpt_builder_suite.distutils.temporary_directory import TemporaryDirectory
-except ImportError:
-    _use_dist_mode = True
-#
-
-def get_version():
+def get_version() -> str:
     """
-Returns the version currently in development.
+    Returns the version currently in development.
 
-:return: (str) Version string
-:since:  v0.1.1
+    :return: (str) Version string
+    :since:  v0.1.1
     """
 
-    return "v1.0.3"
-#
+    return os.environ.get("PPT_VERSION", "0.0.0.dev1")
 
-_setup = { "version": get_version()[1:],
-           "data_files": [ ( "docs", [ "LICENSE", "README" ]) ],
-           "test_suite": "tests"
-         }
 
-if (_use_dist_mode):
-    _setup['package_dir'] = { "": "src" }
-    _setup['packages'] = find_packages("src")
+_setup: Dict[str, Any] = {
+    "version": get_version(),
+    "data_files": [("docs", ["LICENSE", "README"])],
+}
 
-    setup(**_setup)
-else:
-    with TemporaryDirectory(dir = ".") as build_directory:
-        parameters = { "dptJsonVersion": get_version() }
+_setup["package_dir"] = {"": "src"}
+_setup["packages"] = find_packages("src")
 
-        BuildPy.set_build_target_path(build_directory)
-        BuildPy.set_build_target_parameters(parameters)
-
-        Sdist.set_build_target_path(build_directory)
-        Sdist.set_build_target_parameters(parameters)
-
-        package_dir = path.join(build_directory, "src")
-        makedirs(package_dir)
-
-        _setup['package_dir'] = { "": package_dir }
-        _setup['packages'] = [ "dpt_json" ]
-
-        # Customize "cmdclass" to first run builder.py
-        _setup['cmdclass'] = { "build_py": BuildPy, "sdist": Sdist }
-
-        setup(**_setup)
-    #
-#
+setup(**_setup)
--- tests/test_json_resource.py
+++ tests/test_json_resource.py
@@ -1,37 +1,34 @@
 # -*- coding: utf-8 -*-
 
 """
-direct Python Toolbox
-All-in-one toolbox to encapsulate Python runtime variants
+Personal Python Toolkit
+Modularized all-in-one toolkit for Python
 ----------------------------------------------------------------------------
-(C) direct Netware Group - All rights reserved
-https://www.direct-netware.de/redirect?dpt;json
+(C) Tobias "NotTheEvilOne" Wolf - All rights reserved
+https://github.com/NotTheEvilOne/ppt_json
 
 This Source Code Form is subject to the terms of the Mozilla Public License,
 v. 2.0. If a copy of the MPL was not distributed with this file, You can
 obtain one at http://mozilla.org/MPL/2.0/.
-----------------------------------------------------------------------------
-https://www.direct-netware.de/redirect?licenses;mpl2
-----------------------------------------------------------------------------
-unittest
 """
 
 import unittest
 
-from dpt_json import JsonResource
+from ppt_json import JsonResource
+
 
 class TestJsonResource(unittest.TestCase):
     """
-Unittest for JsonResource
+    Unittest for JsonResource
 
-:since: v1.0.0
+    :since: v1.0.0
     """
 
     def _get_json_test_data(self):
         """
-Test data with a simple string and a nested list of content.
+        Test data with a simple string and a nested list of content.
 
-:return: (str) Test data
+        :return: (str) Test data
         """
 
         return """
@@ -40,11 +37,10 @@ Test data with a simple string and a nested list of content.
 "more_complex": [ "this", "that", true, 1 ]
 }
         """
-    #
 
     def test_internal(self):
         """
-Tests the internal JSON parser implementation.
+        Tests the internal JSON parser implementation.
         """
 
         json_resource = JsonResource()
@@ -56,15 +52,14 @@ Tests the internal JSON parser implementation.
         self.assertTrue(json_data is not None)
 
         self.assertTrue("hello" in json_data)
-        self.assertEqual("world", json_data['hello'])
+        self.assertEqual("world", json_data["hello"])
 
         self.assertTrue("more_complex" in json_data)
-        self.assertEqual([ "this", "that", True, 1 ], json_data['more_complex'])
-    #
+        self.assertEqual(["this", "that", True, 1], json_data["more_complex"])
 
     def test_native(self):
         """
-Tests the native JSON Python parser.
+        Tests the native JSON Python parser.
         """
 
         json_data = JsonResource.json_to_data(self._get_json_test_data())
@@ -72,15 +67,14 @@ Tests the native JSON Python parser.
         self.assertTrue(json_data is not None)
 
         self.assertTrue("hello" in json_data)
-        self.assertEqual("world", json_data['hello'])
+        self.assertEqual("world", json_data["hello"])
 
         self.assertTrue("more_complex" in json_data)
-        self.assertEqual([ "this", "that", True, 1 ], json_data['more_complex'])
-    #
+        self.assertEqual(["this", "that", True, 1], json_data["more_complex"])
 
     def test_position_change(self):
         """
-Tests a change to a positional entry.
+        Tests a change to a positional entry.
         """
 
         json_resource = JsonResource()
@@ -93,15 +87,25 @@ Tests a change to a positional entry.
         self.assertEqual("test", json_resource.get_node("more_complex#1"))
 
         json_resource.set_cached_node("more_complex#1")
-        self.assertTrue(json_resource.change_node("more_complex#1", { "some": "unittests", "never": "work" }))
+
+        self.assertTrue(
+            json_resource.change_node(
+                "more_complex#1", {"some": "unittests", "never": "work"}
+            )
+        )
         self.assertEqual("work", json_resource.get_node("more_complex#1 never"))
 
         json_resource.set_cached_node("more_complex#1 never")
-        self.assertTrue(json_resource.change_node("more_complex#1 never", "work but sometimes they do"))
-        self.assertEqual("work but sometimes they do", json_resource.get_node("more_complex#1 never"))
-    #
-#
 
-if (__name__ == "__main__"):
+        self.assertTrue(
+            json_resource.change_node(
+                "more_complex#1 never", "work but sometimes they do"
+            )
+        )
+        self.assertEqual(
+            "work but sometimes they do", json_resource.get_node("more_complex#1 never")
+        )
+
+
+if __name__ == "__main__":
     unittest.main()
-#
